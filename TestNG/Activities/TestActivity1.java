c6df78743899626b73b22779c358b70c44380d02
nidhipal01 <51742096+nidhipal01@users.noreply.github.com>
2025-08-17 22:40:45 +0530

    Add files via upload

---
+++ TestNG/Activities/TestActivity1.java
@@ -0,0 +1,43 @@
+package Activities;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestActivity1 {
+//Declaring the driver
+	WebDriver driver;
+	// Declaring the wait object
+	WebDriverWait wait;
+
+	// setup function
+	@BeforeClass
+	public void setUp() {
+		driver = new FirefoxDriver();
+		wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		driver.get("https://training-support.net");
+	}
+
+	@Test(priority = 1)
+	public void pageTitletest() {
+		Assert.assertEquals(driver.getTitle(), "Training Support");
+		// System.out.println("New page title is: " + driver.getTitle());
+	}
+
+	@Test(priority = 2)
+	public void aboutpageTest() {
+		driver.findElement(By.linkText("About Us")).click();
+		Assert.assertEquals(driver.getTitle(), "About Training Support");
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+}
